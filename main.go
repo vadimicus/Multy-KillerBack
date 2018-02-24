af38afe49d5d7a13ed404c341cddffc5998580b2
vadimicus <vadim.makovsky@gmail.com>
2018-02-24 17:27:23 +0300

    added all sockets events

--- main.go
+++ main.go
@@ -14,6 +14,9 @@ const (
 	EVENT_RECEIVER_ON = "event:receiver:on"
 	EVENT_RECEIVER_ON_OK = "event:receiver:on:ok"
 	EVENT_RECEIVER_OFF = "event:receiver:off"
+	EVENT_SENDER_ON = "event:sender:on"
+	EVENT_SENDER_OFF = "event:sender:off"
+	EVENT_SENDER_UPDATE = "event:sender:update"
 )
 
 func main() {
@@ -29,21 +32,46 @@ func main() {
 		//log.Printf("Some data:", so.Id(), so.Request(), so)
 
 
-		so.On(EVENT_RECEIVER_ON, func(data interface{} ){
+		so.On(EVENT_RECEIVER_ON, func(data interface{} ) string {
 			log.Printf("So id Event RECEIVER ON:", so.Id())
 			log.Printf("DAta:", data)
-			so.Emit(EVENT_RECEIVER_ON_OK, "nice job maaan ;)")
-
+			//so.Emit(EVENT_RECEIVER_ON_OK, "nice job maaan ;)")
+			//TODO add sender logic here
+			return "welcome:to:receiver:side"
 		})
 
-		//so.On("chat message", func(msg string) {
-		//	log.Printf("Some data:", so.Id(), so.Request(), so)
-		//	log.Println("emit:", so.Emit("chat message", msg))
-		//	so.BroadcastTo("chat", "chat message", msg)
-		//})
+		so.On(EVENT_RECEIVER_OFF, func(data interface{} ) string {
+			log.Printf("So id Event RECEIVER OFF:", so.Id())
+			log.Printf("DAta:", data)
+			//so.Emit(EVENT_RECEIVER_ON_OK, "nice job maaan ;)")
+			//TODO add receiver off logic here
+			return "goodbye receiver"
+		})
+
+		so.On(EVENT_SENDER_ON, func(data interface{} ) string {
+			log.Printf("Sender become on:", so.Id())
+			//TODO add sender connect logic here
+			return "hello sender"
+		})
+
+
+		so.On(EVENT_SENDER_OFF, func(data interface{} ) string {
+			log.Printf("Sender become off:", so.Id())
+			//TODO add sender disconnect logic here
+			return "goodbye sender"
+		})
+
+		so.On(EVENT_SENDER_UPDATE, func(data interface{} ) string {
+			log.Printf("Sender update called:", so.Id(), data)
+			//TODO add sender update logic here
+			return "goodbye sender"
+		})
+
+
 		so.On("disconnection", func() {
 			log.Printf("So id:", so.Id())
 			log.Println("on disconnect")
+			//TODO add disconnect socket user logic 
 		})
 	})
 	server.On("error", func(so socketio.Socket, err error) {
