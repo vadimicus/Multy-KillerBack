e8a87018d709cc32bf888c29c93d2300ad1fceba
vadimicus <vadim.makovsky@gmail.com>
2018-02-24 17:06:23 +0300

    added new event

--- main.go
+++ main.go
@@ -9,8 +9,11 @@ import (
 
 const (
 	PORT = ":5555"
-	room_wireless = "wireless"
-	event_connection = "connection"
+	ROOM_WIRELESS = "wireless"
+	EVENT_CONNECTION = "connection"
+	EVENT_RECEIVER_ON = "event:receiver:on"
+	EVENT_RECEIVER_ON_OK = "event:receiver:on:ok"
+	EVENT_RECEIVER_OFF = "event:receiver:off"
 )
 
 func main() {
@@ -18,19 +21,26 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-	server.On(event_connection, func(so socketio.Socket) {
+	server.On(EVENT_CONNECTION, func(so socketio.Socket) {
 		log.Println("on connection")
 		log.Printf("So id:", so.Id())
 		log.Printf("So request", so.Request().GetBody)
-		so.Join(room_wireless)
+		so.Join(ROOM_WIRELESS)
 		//log.Printf("Some data:", so.Id(), so.Request(), so)
 
 
-		so.On("chat message", func(msg string) {
-			log.Printf("Some data:", so.Id(), so.Request(), so)
-			log.Println("emit:", so.Emit("chat message", msg))
-			so.BroadcastTo("chat", "chat message", msg)
+		so.On(EVENT_RECEIVER_ON, func(data interface{} ){
+			log.Printf("So id Event RECEIVER ON:", so.Id())
+			log.Printf("DAta:", data)
+			so.Emit(EVENT_RECEIVER_ON_OK, "nice job maaan ;)")
+
 		})
+
+		//so.On("chat message", func(msg string) {
+		//	log.Printf("Some data:", so.Id(), so.Request(), so)
+		//	log.Println("emit:", so.Emit("chat message", msg))
+		//	so.BroadcastTo("chat", "chat message", msg)
+		//})
 		so.On("disconnection", func() {
 			log.Printf("So id:", so.Id())
 			log.Println("on disconnect")
