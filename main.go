551af22a730fc382d028359722d100b40d5c370d
vadimicus <vadim.makovsky@gmail.com>
2018-04-06 17:49:49 +0300

    added some sender and receiver logic

--- main.go
+++ main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"log"
 	"net/http"
-
 	"github.com/googollee/go-socket.io"
 )
 
@@ -12,10 +11,10 @@ const (
 	ROOM_WIRELESS        = "wireless"
 	EVENT_CONNECTION     = "connection"
 	EVENT_RECEIVER_ON    = "event:receiver:on"
+	EVENT_NEW_RECEIVER	 = "event:new:receiver"
 	//EVENT_RECEIVER_ON_OK = "event:receiver:on:ok"
 	EVENT_RECEIVER_OFF = "event:receiver:off"
 	EVENT_SENDER_ON = "event:sender:on"
-	EVENT_SENDER_OFF = "event:sender:off"
 	EVENT_SENDER_UPDATE = "event:sender:update"
 )
 
@@ -27,13 +26,13 @@ type Receiver struct {
 	CurrencyId	int			`json:"currency_id"`
 	Amount		int64		`json:"amount"`
 	UserCode	int			`json:"user_code"`
-	Socker		*socketio.Socket
+	Socket		*socketio.Socket
 }
 
 type Sender struct {
 	Id			string		`json:"user_id"`
 	UserCode	int			`json:"user_code"`
-	Socker		*socketio.Socket
+	Socket		*socketio.Socket
 }
 
 func main() {
@@ -42,8 +41,9 @@ func main() {
 		log.Fatal(err)
 	}
 
-	//receivers := make(map[int]Receiver)
-	//senders := make(map[int]Sender)
+	receivers := make(map[int]Receiver)
+	senders := []Sender{}
+
 
 	server.On(EVENT_CONNECTION, func(so socketio.Socket) {
 		log.Println("on connection")
@@ -57,7 +57,36 @@ func main() {
 			log.Printf("So id Event RECEIVER ON:", so.Id())
 			log.Printf("DAta:", data)
 			//so.Emit(EVENT_RECEIVER_ON_OK, "nice job maaan ;)")
-			//TODO add sender logic here
+
+			parsed := data.(map[string]interface{})
+			user_id:= parsed["user_id"].(string)
+			user_code:= parsed["user_code"].(int)
+			currency_id := parsed["currency_id"].(int)
+			amount := parsed["amount"].(int64)
+
+			receiver := Receiver{
+				Id:user_id,
+				UserCode:user_code,
+				CurrencyId:currency_id,
+				Amount:amount,
+				Socket: &so,
+			}
+
+
+			_, ok := receivers[receiver.UserCode]
+			if !ok{
+				receivers[receiver.UserCode]=receiver
+			}
+
+			//Try to find Sender
+			for _,sender := range senders{
+				if sender.UserCode == receiver.UserCode{
+					socket := *sender.Socket
+					socket.Emit(EVENT_NEW_RECEIVER, receiver)
+					//sender.Socket.Emit("GOT_YOUR_RECEIVER", receiver)
+				}
+			}
+
 			return "welcome:to:receiver:side"
 		})
 
@@ -75,52 +104,68 @@ func main() {
 
 			//map[string]interface {}=map[user_id:Vadddim user_code:3252])
 			parsed := data.(map[string]interface{})
-			user_id:= parsed["user_id"]
-			user_code:= parsed["user_code"]
+			user_id:= parsed["user_id"].(string)
+			user_code:= parsed["user_code"].(int)
+
+
+
 
 			log.Printf("God data from sender:", user_id, user_code)
 
-			//parsed, ok := data.(Sender)
-			//if ok{
-			//
-			//} else {
-			//	log.Printf("Something went wrong from sender:", data)
-			//}
 
 
-			//TODO parse client from WS and add it to the map
+			sender:= Sender{UserCode:user_code, Id:user_id, Socket:&so }
 
-			//sender := Sender{}
+			var senderExist bool = false
+
+			for _, cachedSender:= range senders{
+				if cachedSender.Id == sender.Id{
+					senderExist = true
+				}
+			}
 
 
+			if !senderExist{
+				senders = append(senders, sender)
+			}
 
-			//senders[client.UserCode]=client
-			//TODO try to find Receiver by the code
-			//receiver, ok := receivers[sender.UserCode]
-			//if ok{
-				//TODO send reciver to sender
-				//TODO and remove this boolshit
-				//receiver.UserCode = client.UserCode
-			//} else{
-				//there is still noreceiver
+			// try to find Receiver by the code
+			receiver, ok := receivers[sender.UserCode]
+			if ok{
+				socket := *sender.Socket
+				socket.Emit(EVENT_NEW_RECEIVER, receiver)
+			} else{
+				var senderExist bool = false
 
-			//}
+				for _, cachedSender:= range senders{
+					if cachedSender.Id == sender.Id{
+						senderExist = true
+					}
+				}
+
+
+				if !senderExist{
+					senders = append(senders, sender)
+				}
+
+
+			}
 
 			return "hello sender"
 		})
 
 
-		so.On(EVENT_SENDER_OFF, func(data interface{} ) string {
-			log.Printf("Sender become off:", so.Id())
-			//TODO add sender disconnect logic here
-			return "goodbye sender"
-		})
-
-		so.On(EVENT_SENDER_UPDATE, func(data interface{} ) string {
-			log.Printf("Sender update called:", so.Id(), data)
-			//TODO add sender update logic here
-			return "goodbye sender"
-		})
+		//so.On(EVENT_SENDER_OFF, func(data interface{} ) string {
+		//	log.Printf("Sender become off:", so.Id())
+		//	//TODO add sender disconnect logic here
+		//	return "goodbye sender"
+		//})
+		//
+		//so.On(EVENT_SENDER_UPDATE, func(data interface{} ) string {
+		//	log.Printf("Sender update called:", so.Id(), data)
+		//	//TODO add sender update logic here
+		//	return "goodbye sender"
+		//})
 
 
 		so.On("disconnection", func() {
