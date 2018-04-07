687cdac9ae7f9be7081fd380585ddb253a0f77a8
vadimicus <vadim.makovsky@gmail.com>
2018-04-07 13:23:41 +0300

    rewrited to new sockets

--- main.go
+++ main.go
@@ -3,7 +3,7 @@ package main
 import (
 	"log"
 	"net/http"
-	"github.com/googollee/go-socket.io"
+
 	"github.com/graarh/golang-socketio"
 	"github.com/graarh/golang-socketio/transport"
 )
@@ -27,14 +27,15 @@ type Receiver struct {
 	Id			string		`json:"user_id"`
 	CurrencyId	int			`json:"currency_id"`
 	Amount		int64		`json:"amount"`
-	UserCode	int			`json:"user_code"`
+	UserCode	string			`json:"user_code"`
+	Socket *gosocketio.Channel
 
 }
 
 type Sender struct {
 	Id			string		`json:"user_id"`
-	UserCode	int			`json:"user_code"`
-
+	UserCode	string			`json:"user_code"`
+	Socket *gosocketio.Channel
 }
 
 func main() {
@@ -54,6 +55,9 @@ func initGraarhSockets()  {
 		Id 		string
 	}
 
+
+	receivers := make(map[string]Receiver)
+	senders := []Sender{}
 
 	user := User{Id:"Vadim Test"}
 
@@ -76,115 +80,88 @@ func initGraarhSockets()  {
 
 
 
-	type Message struct {
-		Name string `json:"name"`
-		Message string `json:"message"`
+	type ReceiverInData struct {
+		Id			string		`json:"user_id"`
+		CurrencyId	int			`json:"currency_id"`
+		Amount		int64		`json:"amount"`
+		UserCode	string			`json:"user_code"`
 
 	}
 
-	//handle custom event
-	server.On(EVENT_SENDER_ON, func(c *gosocketio.Channel, msg Message) string {
-		//send event to all in room
-		//c.BroadcastTo("chat", "message", msg)
+	server.On(EVENT_RECEIVER_ON, func(c *gosocketio.Channel, data ReceiverInData) string {
+		log.Printf("Got messeage Receiver On:", data)
 
-		user.Socket.Emit(EVENT_NEW_RECEIVER, "Send from Sender On")
+		log.Printf("DAta:", data)
+		//so.Emit(EVENT_RECEIVER_ON_OK, "nice job maaan ;)")
+		c.Join(ROOM_WIRELESS)
+
+		receiver:= Receiver{
+			Socket:c,
+			CurrencyId:data.CurrencyId,
+			Amount:data.Amount,
+			Id:data.Id,
+			UserCode:data.UserCode,
+		}
+
+
+
+
+		_, ok := receivers[receiver.UserCode]
+		if !ok{
+			receivers[receiver.UserCode]=receiver
+		}
+
+		//Try to find Sender
+		for _,sender := range senders{
+			if sender.UserCode == receiver.UserCode{
+				sender.Socket.Emit(EVENT_NEW_RECEIVER, receiver)
+			}
+		}
+
 
 		return "OK"
 	})
 
-	//setup http server
-	serveMux := http.NewServeMux()
-	serveMux.Handle("/socket.io/", server)
-	log.Println("Serving at localhost" + PORT)
-	log.Panic(http.ListenAndServe(PORT, serveMux))
-}
-
-
-func initGoogleeSockets(){
-
-	server, err := socketio.NewServer(nil)
-	if err != nil {
-		log.Fatal(err)
+	type SenderInData struct {
+		Code		string		`json:"user_code"`
+		UserId		string		`json:"user_id"`
 	}
 
-	receivers := make(map[int]Receiver)
-	senders := []Sender{}
+
+	//handle custom event
+	server.On(EVENT_SENDER_ON, func(c *gosocketio.Channel, data SenderInData) string {
+
+		log.Printf("Sender become on:", c.Id())
+
+		sender:= Sender{UserCode:data.Code, Id:data.UserId, Socket:c }
 
 
-	server.On(EVENT_CONNECTION, func(so socketio.Socket) {
-		log.Println("on connection")
-		log.Printf("So id:", so.Id())
-		log.Printf("So request", so.Request().GetBody)
 
-		so.Join(ROOM_WIRELESS)
-		so.BroadcastTo(ROOM_WIRELESS, EVENT_NEW_RECEIVER, "HEYYYS can brodcast")
-		//log.Printf("Some data:", so.Id(), so.Request(), so)
+		log.Printf("God data from sender:", sender)
 
-		so.On(EVENT_RECEIVER_ON, func(data interface{} ) string {
-			log.Printf("So id Event RECEIVER ON:", so.Id())
-			log.Printf("DAta:", data)
-			//so.Emit(EVENT_RECEIVER_ON_OK, "nice job maaan ;)")
-			so.Join(so.Id())
+		var senderExist bool = false
 
-			parsed := data.(map[string]interface{})
-			user_id:= parsed["user_id"].(string)
-			raw_code:= parsed["user_code"].(float64)
-			user_code := int(raw_code)
-			raw_currency_id := parsed["currency_id"].(float64)
-			currency_id := int(raw_currency_id)
-			raw_amount := parsed["amount"].(float64)
-			amount := int64(raw_amount)
-
-			receiver := Receiver{
-				Id:user_id,
-				UserCode:user_code,
-				CurrencyId:currency_id,
-				Amount:amount,
+		for _, cachedSender:= range senders{
+			if cachedSender.Id == sender.Id{
+				senderExist = true
 			}
+		}
 
 
-			_, ok := receivers[receiver.UserCode]
-			if !ok{
-				receivers[receiver.UserCode]=receiver
-			}
+		if !senderExist{
+			senders = append(senders, sender)
+		}
 
-			//Try to find Sender
-			for _,sender := range senders{
-				if sender.UserCode == receiver.UserCode{
-					so.BroadcastTo(ROOM_WIRELESS, EVENT_NEW_RECEIVER, "HEY")
-				}
-			}
+		// try to find Receiver by the code
+		receiver, ok := receivers[sender.UserCode]
+		if ok{
+			receiver.Id = "new ID"
+			sender.Socket.Emit(EVENT_NEW_RECEIVER, receiver)
 
-			return "welcome:to:receiver:side"
-		})
-
-		so.On(EVENT_RECEIVER_OFF, func(data interface{} ) string {
-			log.Printf("So id Event RECEIVER OFF:", so.Id())
-			log.Printf("DAta:", data)
-			//so.Emit(EVENT_RECEIVER_ON_OK, "nice job maaan ;)")
-			//TODO add receiver off logic here
-			return "goodbye receiver"
-		})
-
-		so.On(EVENT_SENDER_ON, func(data interface{} ) string {
-			log.Printf("Sender become on:", so.Id())
-
-			//map[string]interface {}=map[user_id:Vadddim user_code:3252])
-			parsed := data.(map[string]interface{})
-			user_id:= parsed["user_id"].(string)
-			raw_code:= parsed["user_code"].(float64)
-			user_code := int(raw_code)
-
-
-
-
-			log.Printf("God data from sender:", user_id, user_code)
-
-
-
-			sender:= Sender{UserCode:user_code, Id:user_id }
-
-			so.BroadcastTo(ROOM_WIRELESS, EVENT_NEW_RECEIVER, "HEyyyy")
+		} else{
+			//TODO remove this shit
+			hardcodeSend := Receiver{UserCode:"32423", Id:"awesome id", Amount:234234, CurrencyId:2,Socket:c}
+			sender.Socket.Emit(EVENT_NEW_RECEIVER, hardcodeSend)
 
 			var senderExist bool = false
 
@@ -199,63 +176,176 @@ func initGoogleeSockets(){
 				senders = append(senders, sender)
 			}
 
-			// try to find Receiver by the code
-			receiver, ok := receivers[sender.UserCode]
-			if ok{
-				receiver.Id = "new ID"
-				so.BroadcastTo(ROOM_WIRELESS, EVENT_NEW_RECEIVER, "Heeey")
-			} else{
-				var senderExist bool = false
 
-				for _, cachedSender:= range senders{
-					if cachedSender.Id == sender.Id{
-						senderExist = true
-					}
-				}
+		}
 
-
-				if !senderExist{
-					senders = append(senders, sender)
-				}
-
-
-			}
-
-			return "hello sender"
-		})
-
-
-		//so.On(EVENT_SENDER_OFF, func(data interface{} ) string {
-		//	log.Printf("Sender become off:", so.Id())
-		//	//TODO add sender disconnect logic here
-		//	return "goodbye sender"
-		//})
-		//
-		//so.On(EVENT_SENDER_UPDATE, func(data interface{} ) string {
-		//	log.Printf("Sender update called:", so.Id(), data)
-		//	//TODO add sender update logic here
-		//	return "goodbye sender"
-		//})
-
-
-		so.On("disconnection", func() {
-			log.Printf("So id:", so.Id())
-			log.Println("on disconnect")
-			//TODO add disconnect socket user logic
-		})
-	})
-	server.On("error", func(so socketio.Socket, err error) {
-		log.Printf("Some data:", so.Id(), so.Request(), so)
-		log.Println("error:", err)
+		return "OK"
 	})
 
-
-
-
-
-
-	http.Handle("/socket.io/", server)
-	//http.Handle("/", http.FileServer(http.Dir("./asset")))
+	//setup http server
+	serveMux := http.NewServeMux()
+	serveMux.Handle("/socket.io/", server)
 	log.Println("Serving at localhost" + PORT)
-	log.Fatal(http.ListenAndServe(PORT, nil))
+	log.Panic(http.ListenAndServe(PORT, serveMux))
 }
+
+
+//func initGoogleeSockets(){
+//
+//	server, err := socketio.NewServer(nil)
+//	if err != nil {
+//		log.Fatal(err)
+//	}
+//
+//	receivers := make(map[int]Receiver)
+//	senders := []Sender{}
+//
+//
+//	server.On(EVENT_CONNECTION, func(so socketio.Socket) {
+//		log.Println("on connection")
+//		log.Printf("So id:", so.Id())
+//		log.Printf("So request", so.Request().GetBody)
+//
+//		so.Join(ROOM_WIRELESS)
+//		so.BroadcastTo(ROOM_WIRELESS, EVENT_NEW_RECEIVER, "HEYYYS can brodcast")
+//		//log.Printf("Some data:", so.Id(), so.Request(), so)
+//
+//		so.On(EVENT_RECEIVER_ON, func(data interface{} ) string {
+//			log.Printf("So id Event RECEIVER ON:", so.Id())
+//			log.Printf("DAta:", data)
+//			//so.Emit(EVENT_RECEIVER_ON_OK, "nice job maaan ;)")
+//			so.Join(so.Id())
+//
+//			parsed := data.(map[string]interface{})
+//			user_id:= parsed["user_id"].(string)
+//			raw_code:= parsed["user_code"].(float64)
+//			user_code := int(raw_code)
+//			raw_currency_id := parsed["currency_id"].(float64)
+//			currency_id := int(raw_currency_id)
+//			raw_amount := parsed["amount"].(float64)
+//			amount := int64(raw_amount)
+//
+//			receiver := Receiver{
+//				Id:user_id,
+//				UserCode:user_code,
+//				CurrencyId:currency_id,
+//				Amount:amount,
+//			}
+//
+//
+//			_, ok := receivers[receiver.UserCode]
+//			if !ok{
+//				receivers[receiver.UserCode]=receiver
+//			}
+//
+//			//Try to find Sender
+//			for _,sender := range senders{
+//				if sender.UserCode == receiver.UserCode{
+//					so.BroadcastTo(ROOM_WIRELESS, EVENT_NEW_RECEIVER, "HEY")
+//				}
+//			}
+//
+//			return "welcome:to:receiver:side"
+//		})
+//
+//		so.On(EVENT_RECEIVER_OFF, func(data interface{} ) string {
+//			log.Printf("So id Event RECEIVER OFF:", so.Id())
+//			log.Printf("DAta:", data)
+//			//so.Emit(EVENT_RECEIVER_ON_OK, "nice job maaan ;)")
+//			//TODO add receiver off logic here
+//			return "goodbye receiver"
+//		})
+//
+//		so.On(EVENT_SENDER_ON, func(data interface{} ) string {
+//			log.Printf("Sender become on:", so.Id())
+//
+//			//map[string]interface {}=map[user_id:Vadddim user_code:3252])
+//			parsed := data.(map[string]interface{})
+//			user_id:= parsed["user_id"].(string)
+//			raw_code:= parsed["user_code"].(float64)
+//			user_code := int(raw_code)
+//
+//
+//
+//
+//			log.Printf("God data from sender:", user_id, user_code)
+//
+//
+//
+//			sender:= Sender{UserCode:user_code, Id:user_id }
+//
+//			so.BroadcastTo(ROOM_WIRELESS, EVENT_NEW_RECEIVER, "HEyyyy")
+//
+//			var senderExist bool = false
+//
+//			for _, cachedSender:= range senders{
+//				if cachedSender.Id == sender.Id{
+//					senderExist = true
+//				}
+//			}
+//
+//
+//			if !senderExist{
+//				senders = append(senders, sender)
+//			}
+//
+//			// try to find Receiver by the code
+//			receiver, ok := receivers[sender.UserCode]
+//			if ok{
+//				receiver.Id = "new ID"
+//				so.BroadcastTo(ROOM_WIRELESS, EVENT_NEW_RECEIVER, "Heeey")
+//			} else{
+//				var senderExist bool = false
+//
+//				for _, cachedSender:= range senders{
+//					if cachedSender.Id == sender.Id{
+//						senderExist = true
+//					}
+//				}
+//
+//
+//				if !senderExist{
+//					senders = append(senders, sender)
+//				}
+//
+//
+//			}
+//
+//			return "hello sender"
+//		})
+//
+//
+//		//so.On(EVENT_SENDER_OFF, func(data interface{} ) string {
+//		//	log.Printf("Sender become off:", so.Id())
+//		//	//TODO add sender disconnect logic here
+//		//	return "goodbye sender"
+//		//})
+//		//
+//		//so.On(EVENT_SENDER_UPDATE, func(data interface{} ) string {
+//		//	log.Printf("Sender update called:", so.Id(), data)
+//		//	//TODO add sender update logic here
+//		//	return "goodbye sender"
+//		//})
+//
+//
+//		so.On("disconnection", func() {
+//			log.Printf("So id:", so.Id())
+//			log.Println("on disconnect")
+//			//TODO add disconnect socket user logic
+//		})
+//	})
+//	server.On("error", func(so socketio.Socket, err error) {
+//		log.Printf("Some data:", so.Id(), so.Request(), so)
+//		log.Println("error:", err)
+//	})
+//
+//
+//
+//
+//
+//
+//	http.Handle("/socket.io/", server)
+//	//http.Handle("/", http.FileServer(http.Dir("./asset")))
+//	log.Println("Serving at localhost" + PORT)
+//	log.Fatal(http.ListenAndServe(PORT, nil))
+//}
